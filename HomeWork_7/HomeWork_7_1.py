982e7878047c1c57027f505c0ba034cbd4900678
Oleksandr <lobov.oleksandr77@gmail.com>
2022-11-30 10:03:49 +0200

    update names

---
+++ HomeWork_7/HomeWork_7_1.py
@@ -0,0 +1,11 @@
+n = []
+try:
+    first = int(input('Enter first number:'))
+    second = int(input('Enter second numbe:'))
+    third = int(input('Enter third number:'))
+    fouth = int(input('Enter fouth number:'))
+    fivth = int(input('Enter fivth number:'))
+    n = [first, second, third, fouth, fivth]
+    print(n[::-1])
+except Exception as e:
+    print('Enter a digit please')
