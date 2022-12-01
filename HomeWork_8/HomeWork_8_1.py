0a21b38cf4b93c1b9f000b59ed3a8bc77e194cdc
Oleksandr <lobov.oleksandr77@gmail.com>
2022-12-01 15:44:52 +0200

    update homeworks 8.1

---
+++ HomeWork_8/HomeWork_8_1.py
@@ -0,0 +1,6 @@
+A = {'Sasha', 'Pasha', 'Igor', 'Dima', 'Viktor', 'Sergey','Stepan', 'Vasya','Vika','Tamara', 'Inna', 'Anna'}
+B = {'Max', 'Evgenii', 'Denis', 'Andrii', 'Stepan', 'Vasya','Sasha', 'Pasha', 'Igor','Anna','Inna','Gliya'}
+
+print(f'Должники за Сентябрь и Октябрь {A} \n  {B}')
+
+print(f'Должники за Октябрь у которых нет долга за Сентябрь {set.difference(B,A)}')
