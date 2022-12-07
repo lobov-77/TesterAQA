9481170c9edf628a358090dc5c0fbe11c87b812a
Oleksandr <lobov.oleksandr77@gmail.com>
2022-12-07 15:32:16 +0200

    update homework 9_4

---
+++ HomeWork_9/HomeWork_9_4.py
@@ -0,0 +1,9 @@
+import random
+file = open('random_numbers.txt', 'w')
+random_number = [random.randint(0, 1000) for _ in range(100)]
+
+fs = [int(x) for x in random_number]
+x = str(fs)
+for x in fs:
+    #for x in range(100):
+    file.write(str(x)+'\n')
