bbb2e8781b651ad498b624f0610bbe6ede99b650
Oleksandr <lobov.oleksandr77@gmail.com>
2022-12-12 23:22:04 +0200

    homework 10_2

--- HomeWork_10/HomeWork_10_2.py
+++ HomeWork_10/HomeWork_10_2.py
@@ -2,4 +2,7 @@ def to_dict(lst):
     return {element: element for element in lst}
 
 
-print(to_dict([23, 35, 'Sasha', 'test', ]))
+x = input('Enter something to list: ')
+y = input('Enter something to list again: ')
+t = input('Enter something to list and one more: ')
+print(to_dict([x, y, t]))
