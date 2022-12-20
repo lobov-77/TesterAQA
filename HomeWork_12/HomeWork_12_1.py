47287bf61c4467f93f44856d345476e1ac8b8c82
Oleksandr <lobov.oleksandr77@gmail.com>
2022-12-20 11:11:14 +0200

    update homework 12_1

--- HomeWork_12/HomeWork_12_1.py
+++ HomeWork_12/HomeWork_12_1.py
@@ -1,14 +1,17 @@
-def is_power_of_two():
-    n = int(input('Enter a number: '))
-    i = 1
-    an = ''
-    while i < n:
-        i = i * 2
-        if i == n:
-            Response = 'YES'
-        else:
-            Response = 'NO'
-    print(f'is_power_of_two ({n}) , "{Response}"')
+def is_power_of_two(number: int, iteration: int = 1):
+    new_number = number / 2
+
+    _result = False
+
+    if new_number == 2:
+        return True
+
+    elif new_number.is_integer():
+        _result = is_power_of_two(new_number, iteration + 1)
+
+    return _result
 
 
-is_power_of_two()
+result = is_power_of_two(125)
+
+print('yes' if result else 'no')
