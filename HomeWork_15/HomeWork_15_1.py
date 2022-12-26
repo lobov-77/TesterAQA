c5ef02f6a49e77c5b37541809d8853036ee50a28
Oleksandr <lobov.oleksandr77@gmail.com>
2022-12-26 18:46:31 +0200

    update homework 15_1

--- HomeWork_15/HomeWork_15_1.py
+++ HomeWork_15/HomeWork_15_1.py
@@ -1,4 +1,27 @@
-def reverse_iterator(source: list) -> iter:
+class reverse_iterator:
+    def __init__(self):
+        self.data = None
+        self.size = None
+        self.index = None
+
+    def __init__(self, sq: list) -> None:
+        self.data = sq[::-1]
+        self.size = len(sq)
+        self.index = -1
+
+    def __next__(self):
+        if self.index < self.size - 1:
+            self.index += 1
+            return self.data[self.index]
+        else:
+            raise StopIteration
+
+    def __iter__(self):
+        self.index = -1
+        return self
+
+
+def rev_iterator(source: list) -> iter:
     return iter(source[::-1])
 
 
