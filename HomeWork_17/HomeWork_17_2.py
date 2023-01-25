295fb54ffc83a82f8ea7c887663b31bb07f79987
Oleksandr <lobov.oleksandr77@gmail.com>
2023-01-25 12:03:41 +0200

    update homework 17

--- HomeWork_17/HomeWork_17_2.py
+++ HomeWork_17/HomeWork_17_2.py
@@ -1,37 +1,63 @@
+import pytest
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common import actions
 from selenium.webdriver.common.by import By
+from selenium.webdriver import Chrome
 
-FULL_NAME_FIELD = 'input[id=userName]'
-EMAIL_FIELD = 'input[id=userEmail]'
-CURRENT_ADDRESS_FIELD = 'textarea[id=currentAddress]'
-PERMANENT_ADDRESS_FIELD = 'textarea[id=permanentAddress]'
-SUBMIT_BUTTON = 'button[id=submit]'
+FULL_NAME_FIELD = 'input#userName'
+EMAIL_FIELD = 'input#userEmail'
+CURRENT_ADDRESS_FIELD = 'textarea#currentAddress'
+PERMANENT_ADDRESS_FIELD = 'textarea#permanentAddress'
+SUBMIT_BUTTON = 'button#submit'
+
+driver = Chrome()
 
 
-def test_form(chrome):
-    driver = chrome
+@pytest.fixture
+def text_fields():
     driver.get('https://demoqa.com/text-box')
-    name = driver.find_element(By.CSS_SELECTOR, FULL_NAME_FIELD)
-    email = driver.find_element(By.CSS_SELECTOR, EMAIL_FIELD)
-    current_address = driver.find_element(By.CSS_SELECTOR, CURRENT_ADDRESS_FIELD)
-    permanent_address = driver.find_element(By.CSS_SELECTOR, PERMANENT_ADDRESS_FIELD)
-    submit = driver.find_element(By.CSS_SELECTOR, SUBMIT_BUTTON)
 
-    name.send_keys('Oleksandr Lobov')
-    email.send_keys('test@gmail.com')
-    current_address.send_keys('Cherkasy Gogolya str.')
-    permanent_address.send_keys('Cherkasy Himka prov.')
+
+def test_form(text_fields):
+    driver.get('https://demoqa.com/text-box')
+    name_field = driver.find_element(By.CSS_SELECTOR, FULL_NAME_FIELD)
+    email_field = driver.find_element(By.CSS_SELECTOR, EMAIL_FIELD)
+    current_address_field = driver.find_element(By.CSS_SELECTOR, CURRENT_ADDRESS_FIELD)
+    permanent_address_field = driver.find_element(By.CSS_SELECTOR, PERMANENT_ADDRESS_FIELD)
+    submit_button = driver.find_element(By.CSS_SELECTOR, SUBMIT_BUTTON)
+
+    name_field.send_keys('Oleksandr Lobov')
+    email_field.send_keys('test@gmail.com')
+    current_address_field.send_keys('Cherkasy Gogolya str.')
+    permanent_address_field.send_keys('Cherkasy Himka prov.')
     driver.execute_script('window.scrollTo(0, 500)')
-    submit.click()
+    submit_button.click()
 
-    name_text = name.get_attribute('value')
-    email_text = email.get_attribute('value')
-    curr_add_text = current_address.get_attribute('velue')
-    per_add_text = permanent_address.get_attribute('velue')
+    name_filed_text = name_field.get_attribute('value')
+    email_filed_text = email_field.get_attribute('value')
+    curr_add_filed_text = current_address_field.get_attribute('value')
+    per_add_filed_text = permanent_address_field.get_attribute('value')
 
-    result_name = driver.find_element(By.CSS_SELECTOR, 'p[id=name]').text
-    result_email = driver.find_element(By.CSS_SELECTOR, 'p[id=email]').text
-    result_ca = driver.find_element(By.CSS_SELECTOR, 'p[id="currentAddress"]').text
-    result_pa = driver.find_element(By.CSS_SELECTOR, 'p[id="permanentAddress"]').text
-    pass
+    result_name_filed = driver.find_element(By.CSS_SELECTOR, 'p[id=name]').text.split(':')[1]
+    result_email_filed = driver.find_element(By.CSS_SELECTOR, 'p[id=email]').text.split(':')[1]
+    result_ca_filed = driver.find_element(By.CSS_SELECTOR, 'p[id="currentAddress"]').text.split(':')[1]
+    result_pa_filed = driver.find_element(By.CSS_SELECTOR, 'p[id="permanentAddress"]').text.split(':')[1]
+
+    assert_data_fields = [result_name_filed == name_filed_text, result_email_filed == email_filed_text,
+                          result_ca_filed == curr_add_filed_text,
+                          result_pa_filed == per_add_filed_text]
+
+    assert all(assert_data_fields)
+
+
+def invalid_email(text_fields):
+    driver.get('https://demoqa.com/text-box')
+    email_field = driver.find_element(By.CSS_SELECTOR, EMAIL_FIELD)
+    submit_button = driver.find_element(By.CSS_SELECTOR, SUBMIT_BUTTON)
+
+    email_field.send_keys('test')
+    driver.execute_script('window.scrollTo(0, 500)')
+    submit_button.click()
+
+    email_field_error = driver.find_element(By.CSS_SELECTOR, 'input[class="mr-sm-2 field-error form-control"]')
+    assert email_field_error.is_displayed()
--- HomeWork_17/test_HomeWork_17_1.py
+++ HomeWork_17/test_HomeWork_17_1.py
@@ -1,6 +1,8 @@
+import pytest
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common import actions
 from selenium.webdriver.common.by import By
+from selenium.webdriver import Chrome
 
 FULL_NAME_FIELD = '//input[@id="userName"]'
 EMAIL_FIELD = '//input[@id="userEmail"]'
@@ -8,30 +10,55 @@ CURRENT_ADDRESS_FIELD = '//textarea[@id="currentAddress"]'
 PERMANENT_ADDRESS_FIELD = '//textarea[@id="permanentAddress"]'
 SUBMIT_BUTTON = '//button[@id="submit"]'
 
+driver = Chrome()
 
-def test_form(chrome):
-    driver = chrome
+
+@pytest.fixture
+def text_fields():
     driver.get('https://demoqa.com/text-box')
-    name = driver.find_element(By.XPATH, FULL_NAME_FIELD)
-    email = driver.find_element(By.XPATH, EMAIL_FIELD)
-    current_address = driver.find_element(By.XPATH, CURRENT_ADDRESS_FIELD)
-    permanent_address = driver.find_element(By.XPATH, PERMANENT_ADDRESS_FIELD)
-    submit = driver.find_element(By.XPATH, SUBMIT_BUTTON)
 
-    name.send_keys('Oleksandr Lobov')
-    email.send_keys('test@gmail.com')
-    current_address.send_keys('Cherkasy Gogolya str.')
-    permanent_address.send_keys('Cherkasy Himka prov.')
+
+def test_form(text_fields):
+    name_field = driver.find_element(By.XPATH, FULL_NAME_FIELD)
+    email_field = driver.find_element(By.XPATH, EMAIL_FIELD)
+    current_address_field = driver.find_element(By.XPATH, CURRENT_ADDRESS_FIELD)
+    permanent_address_field = driver.find_element(By.XPATH, PERMANENT_ADDRESS_FIELD)
+    submit_button = driver.find_element(By.XPATH, SUBMIT_BUTTON)
+
+    name_field.send_keys('Oleksandr Lobov')
+    email_field.send_keys('test@gmail.com')
+    current_address_field.send_keys('Cherkasy Gogolya str.')
+    permanent_address_field.send_keys('Cherkasy Himka prov.')
     driver.execute_script('window.scrollTo(0, 500)')
-    submit.click()
+    submit_button.click()
 
-    name_text = name.get_attribute('value')
-    email_text = email.get_attribute('value')
-    curr_add_text = current_address.get_attribute('velue')
-    per_add_text = permanent_address.get_attribute('velue')
+    name_field_text = name_field.get_attribute('value')
+    email_field_text = email_field.get_attribute('value')
+    curr_add_field_text = current_address_field.get_attribute('value')
+    per_add_field_text = permanent_address_field.get_attribute('value')
 
-    result_name = driver.find_element(By.ID, 'name').text
-    result_email = driver.find_element(By.ID, 'email').text
-    result_ca = driver.find_element(By.XPATH, '//p[@id="currentAddress"]').text
-    result_pa = driver.find_element(By.XPATH, '//p[@id="permanentAddress"]').text
-    pass
+    result_name_field = driver.find_element(By.ID, 'name').text.split(':')[1]
+    result_email_field = driver.find_element(By.ID, 'email').text.split(':')[1]
+    result_ca_field = driver.find_element(By.XPATH, '//p[@id="currentAddress"]').text.split(':')[1]
+    result_pa_field = driver.find_element(By.XPATH, '//p[@id="permanentAddress"]').text.split(':')[1]
+
+    assert_data_fields = [result_name_field == name_field_text, result_email_field == email_field_text,
+                          result_ca_field == curr_add_field_text,
+                          result_pa_field == per_add_field_text]
+    assert all(assert_data_fields)
+
+
+def invalid_email(text_fields):
+    driver.get('https://demoqa.com/text-box')
+    email_field = driver.find_element(By.XPATH, EMAIL_FIELD)
+    submit_button = driver.find_element(By.XPATH, SUBMIT_BUTTON)
+
+    email_field.send_keys('test')
+    driver.execute_script('window.scrollTo(0, 500)')
+    submit_button.click()
+
+    email_field_error = driver.find_element(By.XPATH, '//input[@class="mr-sm-2 field-error form-control"]')
+    assert email_field_error.is_displayed()
+
+
+
